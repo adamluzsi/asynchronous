2a6d79fc7f529870a9c2d2e60d635d01f0f9afe1
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-03-03 17:13:22 +0100

    coding standards for clean class

--- lib/asynchronous/clean_class.rb
+++ lib/asynchronous/clean_class.rb
@@ -2,11 +2,13 @@ module Asynchronous
   class CleanClass < BasicObject
 
     # remove methods from the class!
-    (self.instance_methods-[
-        :object_id,
-        :__send__,
-        :alias,
-    ]).each do |method|
+    (
+      self.instance_methods-[
+          :object_id,
+          :__send__,
+          :alias,
+      ]
+    ).each do |method|
       undef_method method
     end
 
